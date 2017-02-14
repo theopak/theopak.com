3d5ac67a1125445c53bdcd577c904686f5cd1c87
Theodore X. Pak <theopak@gmail.com>
2017-02-13 19:36:12 -0500

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,11 @@
-var harp        = require('harp');
-var gulp        = require('gulp');
-var deploy      = require('gulp-gh-pages');
-var cp          = require('child_process');
+var gulp = require('gulp')
+var htmlmin = require('gulp-htmlmin')
 
-/**
- * Build the Harp Site
- */
-gulp.task('build', function (done) {
-  cp.exec('harp compile . dist', {stdio: 'inherit'})
-    .on('close', done)
-});
-
-/**
- * Push build to gh-pages
- */
-gulp.task('deploy', ['build'], function () {
-  return gulp.src("./dist/**/*")
-    .pipe(deploy())
-});
+gulp.task('minify', function() {
+ return gulp.src('src/*.html')
+   .pipe(htmlmin({
+     collapseWhitespace: true,
+     minifyCSS: true
+   }))
+   .pipe(gulp.dest('docs'))
+})
